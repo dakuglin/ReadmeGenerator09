fb9872ff909c2e7274788beb382eeaf8d90e72a6
Dana Kuglin <dakuglin@gmail.com>
2020-10-13 19:53:15 -0600

    updated readme page layout

--- Develop/README_GEN.md
+++ Develop/README_GEN.md
@@ -1,9 +1,24 @@
-# 
-# Description
-# Table Of Contents
-# Installation
-# Usage
-# License
-# Contrubuting
-# Tests
-# Questions
+# Please work
+
+## Description  
+Please please work
+
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+  
+## Installation
+
+## Usag 
+
+## License
+
+## Contrubuting
+
+## Tests
+
+## Questions
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,17 +1,44 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
 
-  return [`# ${data.title}` + "\n" + `# Description` + "\n" + `# Table Of Contents`
-  + "\n" + `# Installation` + "\n" + `# Usage` + "\n" + `# License` + "\n" + `# Contrubuting` + "\n" + `# Tests` + "\n" + `# Questions`];
-   
+ return `# ${data.title}
+
+## Description  
+${data.description}
+
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+  
+## Installation
+
+## Usag 
+
+## License
+
+## Contrubuting
+
+## Tests
+
+## Questions`; 
+  
 }
+
+
+
+
 
 module.exports = generateMarkdown ;
 
 
 
 
-// ask all the questions 
+
+//do the questions 
 //start generating the readme file
     //loop process
     //what item of readme am I doing first...next...what am I writing (subheading, link)
@@ -19,3 +46,7 @@ module.exports = generateMarkdown ;
     //add markdown code to the readme file
 
     //screen to gif instead of the video 
+
+
+    
+     
